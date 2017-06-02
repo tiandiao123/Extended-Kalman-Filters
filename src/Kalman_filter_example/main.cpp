46fa96d771b38f45ef8b71db62f8a7882ad60f9d
Cuiqing Li <lixx3527@umn.edu>
2017-06-02 02:53:29 -0400

    working on C++ implementation

--- src/Kalman_filter_example/main.cpp
+++ src/Kalman_filter_example/main.cpp
@@ -30,7 +30,7 @@ int main() {
 	string line;
 	// set i to get only first 3 measurments
 	int i = 0;
-	while(getline(in_file, line) && (i<=3)){
+	while(getline(in_file, line) && (i<=100)){
 
 		MeasurementPackage meas_package;
 
